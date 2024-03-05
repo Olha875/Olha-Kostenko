6c3ccc49c929c88bb8211cee07ecbabcb9b36c67
Olha875 <59772930+Olha875@users.noreply.github.com>
2024-03-05 21:40:22 +0200

    lesson14

---
+++ src/main/java/com/example/hillel/lesson14/Pass.java
@@ -0,0 +1,35 @@
+package com.example.hillel.lesson14;
+
+
+public class Pass {
+
+  private String password;
+
+  public Pass(String password) {
+    this.password = password;
+  }
+
+  public String validatePassword() throws WeakPasswordException {
+
+    String validation = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]+$";
+
+    if (!password.matches(validation)) {
+      return "Password validation failed: must contain at least one lowercase letter, one uppercase letter, and one digit";
+    }
+    if (password.length() < 8) {
+      throw new WeakPasswordException("Password validation failed: Password must be at least 8 characters long");
+    }
+    return "Password is valid";
+  }
+
+  public static void main(String[] args) {
+    String password = "Ww2";
+    Pass pass = new Pass(password);
+    try {
+      String validationResult = pass.validatePassword();
+      System.out.println(validationResult);
+    } catch (WeakPasswordException e) {
+      System.out.println("Fail: " + e.getMessage());
+    }
+  }
+}
---
+++ src/main/java/com/example/hillel/lesson14/WeakPasswordException.java
@@ -0,0 +1,7 @@
+package com.example.hillel.lesson14;
+
+public class WeakPasswordException extends Exception {
+  public WeakPasswordException(String message) {
+    super(message);
+  }
+}
